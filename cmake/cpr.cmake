e3432db800d7b6520d2578e67bda9b6f5732d5b3
anyi <anyi@a2r6.cn>
2026-03-24 02:33:19 +0800

    修复登录后不显示课程问题 修复刷课不起作用 修复部分报错问题

--- cmake/cpr.cmake
+++ cmake/cpr.cmake
@@ -1,4 +1,4 @@
-include(FetchContent)
+﻿include(FetchContent)
 
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "" FORCE)
 set(CPR_USE_SYSTEM_CURL OFF CACHE BOOL "" FORCE)
@@ -6,6 +6,6 @@ set(CPR_USE_SYSTEM_CURL OFF CACHE BOOL "" FORCE)
 FetchContent_Declare(
     cpr
     GIT_REPOSITORY https://github.com/libcpr/cpr.git
-    GIT_TAG 1.11.0
+    GIT_TAG 1.14.2
 )
 FetchContent_MakeAvailable(cpr)
